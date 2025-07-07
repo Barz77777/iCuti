30f0f0ad0c48c0450c0dd2b109d5025fd34f1390
makbarenurhermawan@gmail.com <makbarenurhermawan@gmail.com>
2025-07-07 10:48:22 +0700

    menambahkan perbaikan bugs

--- public/app/controller/icuti_bot_status.php
+++ public/app/controller/icuti_bot_status.php
@@ -1,27 +1,30 @@
 <?php
 function kirimTelegram($pesan)
 {
-    $token = "7283511005:AAFjGPP1EHd9MBRHlP8MXHWVfnq29X2xTkc"; // ganti dengan token bot kamu
-    $chat_id =     $chat_id = [
-        "7324689890",
-        "1826576117"
-    ]; //ID Chat
+    $token = "7283511005:AAFjGPP1EHd9MBRHlP8MXHWVfnq29X2xTkc"; // Ganti token bot kamu
 
-    $url = "https://api.telegram.org/bot{$token}/sendMessage";
-    $data = [
-        'chat_id' => $chat_id,
-        'text' => $pesan,
-        'parse_mode' => 'HTML'
+    $chat_ids = [
+        "7324689890", // Chat ID kamu
+        "1826576117"  // Chat ID teman kamu
     ];
 
-    $options = [
-        "http" => [
-            "method"  => "POST",
-            "header"  => "Content-Type:application/x-www-form-urlencoded",
-            "content" => http_build_query($data),
-        ],
-    ];
+    foreach ($chat_ids as $chat_id) {
+        $url = "https://api.telegram.org/bot{$token}/sendMessage";
+        $data = [
+            'chat_id' => $chat_id,
+            'text' => $pesan,
+            'parse_mode' => 'HTML'
+        ];
 
-    $context = stream_context_create($options);
-    file_get_contents($url, false, $context);
+        $options = [
+            "http" => [
+                "method"  => "POST",
+                "header"  => "Content-Type:application/x-www-form-urlencoded",
+                "content" => http_build_query($data),
+            ],
+        ];
+
+        $context = stream_context_create($options);
+        @file_get_contents($url, false, $context); // Tambahkan @ untuk hindari warning error langsung ke output
+    }
 }
