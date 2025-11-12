c04dd81923d98e8ec73c0550b85e247765d39512
makbarenurhermawan@gmail.com <makbarenurhermawan@gmail.com>
2025-11-12 15:43:33 +0700

    update

--- public/app/view/admin/beranda-atasan-agreement.php
+++ public/app/view/admin/beranda-atasan-agreement.php
@@ -46,12 +46,12 @@ $jumlahNotifBaru = $resJumlah->fetch_assoc()['total'] ?? 0;
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" href="/asset/iC.png">
-    <link rel="icon" href="/asset/user-avatar.png">
+    <link rel="icon" href="/public/asset/iC.png">
+    <link rel="icon" href="/public/asset/user-avatar.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-    <link rel="stylesheet" href="/public/style/beranda-atasan-agreement.css" />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/public/style/beranda-atasan-agreement.css">
     <script>
         tailwind.config = {
             theme: {
--- public/app/view/user/beranda-user-submission.php
+++ public/app/view/user/beranda-user-submission.php
@@ -86,11 +86,11 @@ $jenisCuti = $result->fetch_assoc();
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="icon" href="/asset/iC.png">
+  <link rel="icon" href="/public/asset/iC.png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="/public/style/beranda-user-submission.css" />
+  <link rel="stylesheet" href="/public/style/beranda-user-submission.css">
   <script>
     tailwind.config = {
       theme: {
